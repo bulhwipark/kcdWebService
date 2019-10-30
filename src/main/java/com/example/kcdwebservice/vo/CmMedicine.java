82064bf5d54ce8aaa5ce7e0ecca12f00e8c7b319
dgkim <dgkim@saltlux.com>
2019-10-30 14:20:02 +0900

    cm_medicien table vo

---
+++ src/main/java/com/example/kcdwebservice/vo/CmMedicine.java
@@ -0,0 +1,30 @@
+package com.example.kcdwebservice.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class CmMedicine {
+    private String kdCd;
+    private String prinipalCompCd;
+    private String drugNmKor;
+    private String drugManuf;
+    private String hiraAtcCd;
+    private String hiraAtcNm;
+    private String stdCd;
+    private String drugNmEng;
+    private String substanceNm;
+    private String amount1;
+    private String unit1;
+    private String eftSubstNm;
+    private String amount2;
+    private String unit2;
+    private String amount3;
+    private String unit3;
+    private String medDoseFrm;
+    private String rtOfAdmin;
+    
+}
