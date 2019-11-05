b8a898f8f0bf10a782584b4138f6471bdc64b592
김동건 <dgkim@saltlux.com>
2019-11-05 18:12:55 +0900

    auto rule 1

--- src/main/java/com/example/kcdwebservice/controller/MainController.java
+++ src/main/java/com/example/kcdwebservice/controller/MainController.java
@@ -200,24 +200,52 @@ public class MainController {
     public String autoMapKcdRule2(@PathVariable("id") String ruleTp) {
         ruleMapService.selectMediList(ruleTp);
 
-        //ruleMapService.selectMediList("10");
-        // ruleMapService.selectMediList("11");
-        //ruleMapService.selectMediList("12");
-        //  ruleMapService.selectMediList("13");
-        // ruleMapService.selectMediList("20");
-        // ruleMapService.selectMediList("21");
-        // ruleMapService.selectMediList("22");
-        // ruleMapService.selectMediList("23");
-        // ruleMapService.selectMediList("30");
-        // ruleMapService.selectMediList("31");
-        // ruleMapService.selectMediList("32");
-        // ruleMapService.selectMediList("33");
-        // ruleMapService.selectMediList("40");
-        // ruleMapService.selectMediList("41");
-        // ruleMapService.selectMediList("42");
-        // ruleMapService.selectMediList("43");
+    //     ruleMapService.selectMediList("10");
+    //     ruleMapService.selectMediList("11");
+    //     ruleMapService.selectMediList("12");
+    //      ruleMapService.selectMediList("13");
+    //     ruleMapService.selectMediList("20");
+    //     ruleMapService.selectMediList("21");
+    //     ruleMapService.selectMediList("22");
+    //     ruleMapService.selectMediList("23");
+    //     ruleMapService.selectMediList("30");
+    //     ruleMapService.selectMediList("31");
+    //     ruleMapService.selectMediList("32");
+    //     ruleMapService.selectMediList("33");
+    //     ruleMapService.selectMediList("40");
+    //     ruleMapService.selectMediList("41");
+    //     ruleMapService.selectMediList("42");
+    //     ruleMapService.selectMediList("43");
     //    ruleMapService.selectMediList("5");
     //    ruleMapService.selectMediList("6");
+
+    
+       return "자동룰매핑종료"+System.currentTimeMillis();
+    }
+
+    @GetMapping(value="/mediRule2")
+    @ResponseBody
+    public String autoMapKcdRule3(@PathVariable("id") String ruleTp) {
+       //ruleMapService.selectMediList(ruleTp);
+
+        ruleMapService.selectMediList("10");
+        ruleMapService.selectMediList("11");
+        ruleMapService.selectMediList("12");
+         ruleMapService.selectMediList("13");
+        ruleMapService.selectMediList("20");
+        ruleMapService.selectMediList("21");
+        ruleMapService.selectMediList("22");
+        ruleMapService.selectMediList("23");
+        ruleMapService.selectMediList("30");
+        ruleMapService.selectMediList("31");
+        ruleMapService.selectMediList("32");
+        ruleMapService.selectMediList("33");
+        ruleMapService.selectMediList("40");
+        ruleMapService.selectMediList("41");
+        ruleMapService.selectMediList("42");
+        ruleMapService.selectMediList("43");
+       ruleMapService.selectMediList("5");
+       ruleMapService.selectMediList("6");
 
     
        return "자동룰매핑종료"+System.currentTimeMillis();
