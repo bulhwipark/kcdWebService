d270ffc9d1662b437fa0715d1f87df93eda9e9ca
julia <julia1450@naver.com>
2019-12-02 16:35:59 +0900

    웹 상에서 mapping 된 값을 여러가지 형식으로 가져오기 위해 domapping 외에 다른 로직 추가

--- src/main/java/com/example/kcdwebservice/service/CmKexamService.java
+++ src/main/java/com/example/kcdwebservice/service/CmKexamService.java
@@ -7,6 +7,7 @@ import com.example.kcdwebservice.vo.MapKcdSctVo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONArray;
+import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.stereotype.Service;
 
@@ -47,6 +48,14 @@ public class CmKexamService {
 	public List<CmKexamVo> kexam_selectNoMapping(CmKexamVo cmKexamVo) {
 		return cmKexamDao.kexam_selectNoMapping(cmKexamVo);
 	}
+	
+	public List<CmKexamVo> getMediCheckInfoList(String kexCd){
+		return cmKexamDao.getMediCheckInfoList(kexCd);
+	}
+	
+	public List<CmKexamVo> mediCheckDetailList(String kexCd){
+		return cmKexamDao.mediCheckDetailList(kexCd);
+	}
 
 	public String kexam_totalCnt(String mappingStatus, CmKexamVo cmKexamVo) {
 		//검사 총 건수 리턴
@@ -65,11 +74,11 @@ public class CmKexamService {
 		// 매핑된 검사 건수만 리턴
 		String totalCnt = null;
 		if (mappingStatus.equals("All")) {
-			totalCnt = cmKexamDao.kexam_totalCnt(cmKexamVo);
+			totalCnt = cmKexamDao.kexam_kexCdTotalCnt(cmKexamVo);
 		} else if (mappingStatus.equals("Mapping")) {
-			totalCnt = cmKexamDao.kexam_mappingTotalCnt(cmKexamVo);
+			totalCnt = cmKexamDao.kexam_kexCdMappingTotalCnt(cmKexamVo);
 		} else if (mappingStatus.equals("NoMapping")) {
-			totalCnt = cmKexamDao.kexam_noMappingTotalCnt(cmKexamVo);
+			totalCnt = cmKexamDao.kexam_kexCdNoMappingTotalCnt(cmKexamVo);
 		}
 		return totalCnt;
 	}
@@ -109,6 +118,90 @@ public class CmKexamService {
 		else if(roman.equals("\u217F")) term = term.replace(roman, "M");
 		
 		return term;
+	}
+	
+	public JSONObject searchTermObject(String term, String ecl) {
+		String strUrl = "http://1.224.169.78:8095/MAIN/concepts?";
+		
+		Pattern pat = Pattern.compile("[\\u2160-\\u217B]");
+		Matcher match = pat.matcher(term);
+		while(match.find()) {
+			String roman = match.group();
+			term = convertRomanToChar(term, roman);
+			match = pat.matcher(term);
+		}
+
+		Map<String, String> hm = new HashMap<String, String>();
+		hm.put("activeFilter", "true");
+		hm.put("termActive", "true");
+		hm.put("statedEcl", ecl);
+		hm.put("term", term.replaceAll("[\\{\\}\\[\\]\\/?.,;:|\\)*~`!^\\-_+<>@\\#$%&\\\\\\=\\(\\'\\\"]", " "));
+
+		try {
+			JSONObject jobj = new JSONObject(com.example.kcdwebservice.util.HttpRestCall.callGet(strUrl, hm));
+			System.out.println("out:" + jobj);
+			JSONArray ja = jobj.getJSONArray("items");
+
+			for (int x = 0; x < ja.length(); x++) {
+				// db에서 conceptid에 해당하는 모든 동의어를 lowercase로 변경하여 term 과 일치하는지 확인
+				JSONObject jo = ja.getJSONObject(x);
+				String sctId = jo.getString("id");
+
+				// 유사어도 모두 확인해야함. gun
+				List<String> lstSCTSynon = cmKexamDao.selectSynonym(sctId);
+
+				for (String strTerm : lstSCTSynon) {
+					if (term.replace(" ", "").toLowerCase().equals(strTerm.replace(" ", "").toLowerCase())) {
+						return jo;
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public List<JSONObject> searchTermList(String term, String ecl){
+		ArrayList<JSONObject> arrSctid = new ArrayList<JSONObject>();
+
+	    String strUrl = "http://1.224.169.78:8095/MAIN/concepts?";
+	    
+	    Pattern pat = Pattern.compile("[\\u2160-\\u217B]");
+		Matcher match = pat.matcher(term);
+		while(match.find()) {
+			String roman = match.group();
+			term = convertRomanToChar(term, roman);
+			match = pat.matcher(term);
+		}
+
+	    Map<String, String> hm = new HashMap<String, String>();
+
+	    hm.put("activeFilter", "true");
+	    hm.put("termActive", "true");
+	    hm.put("statedEcl", ecl);
+	    hm.put("term", term.replaceAll("[\\{\\}\\[\\]\\/?.,;:|\\)*~`!^\\-_+<>@\\#$%&\\\\\\=\\(\\'\\\"]", " "));
+
+	    try {
+	      JSONObject jobj = new JSONObject(com.example.kcdwebservice.util.HttpRestCall.callGet(strUrl, hm));
+	      JSONArray ja = jobj.getJSONArray("items");
+
+	      for (int x = 0; x < ja.length(); x++) {
+	        JSONObject jo = ja.getJSONObject(x);
+	        String sctId = jo.getString("id");
+	        // 유사어도 모두 확인해야함. gun
+			List<String> lstSCTSynon = cmKexamDao.selectSynonym(sctId);
+
+			for (String strTerm : lstSCTSynon) {
+				if (term.replace(" ", "").toLowerCase().equals(strTerm.replace(" ", "").toLowerCase())) {
+					arrSctid.add(jo);
+				}
+			}
+	      }
+	    } catch (Exception e) {
+	      e.printStackTrace();
+	    }
+	    return arrSctid;
 	}
 
 	public String searchTerm(String term, String ecl) {
@@ -355,6 +448,122 @@ public class CmKexamService {
 			if (isMatched)
 				break;
 		}
+	}
+	
+	public List<JSONObject> autoRuleMapping(List<CmKexamVo> mediCheckInfoList) {
+		List<JSONObject> resultList = new ArrayList<JSONObject>();
+		String targetSentence = mediCheckInfoList.get(0).getKexEng();
+		Map<Integer, String> hm = getPatternMap();
+		Collection<Integer> keySet = hm.keySet();
+		Iterator<Integer> itr = keySet.iterator();
+		boolean isMatched = false;
+		while (itr.hasNext()) {
+			int key = itr.next();
+			if (key == 1 || key == 21 || key == 22) {
+				Pattern pat = Pattern.compile(hm.get(key));
+				Matcher match = pat.matcher(targetSentence);
+				while (match.find()) {
+					// 정규식 적용했으니까 key 값에 따라 결과 정제하고 term 찾기
+					String normalizeWord = match.group();
+					if (normalizeWord.isEmpty())
+						continue;
+					if (key == 21)
+						normalizeWord = normalizeWord.replace("(", "").replace(")", "");
+					else if (key == 22) {
+//						normalizeWord = normalizeWord.substring(1);
+						normalizeWord = normalizeWord.split("-")[normalizeWord.split("-").length-1];
+					}
+						
+					JSONObject searchResult = searchTermObject(normalizeWord, "<71388002");
+					if (searchResult!=null) {
+						// flag true로 설정
+						isMatched = true;
+						resultList.add(searchResult);
+						break;
+					}
+				}
+			} else if (key == 31 || key == 32 || key == 33 || key == 34 || key == 35 || key == 36 || key == 41
+					|| key == 42 || key == 43 || key == 44 || key == 45 || key == 46 || key == 5) {
+				Pattern pat = Pattern.compile(hm.get(key));
+				Matcher match = pat.matcher(targetSentence);
+				if (match.find()) {
+					String normalizeWord = targetSentence.replaceAll(hm.get(key), "");
+
+					JSONObject searchResult = searchTermObject(normalizeWord, "<71388002");
+					if (searchResult != null) {
+						// flag true로 설정
+						isMatched = true;
+						resultList.add(searchResult);
+						break;
+					}
+				}
+
+			} else if (key == 61 || key == 62 || key == 63 || key == 64 || key == 65 || key == 66 || key == 67
+					|| key == 68 || key == 69 || key == 610) {
+				String normalizeWord = hm.get(key).replace("{}", targetSentence);
+
+				JSONObject searchResult = searchTermObject(normalizeWord, "<71388002");
+				if (searchResult != null) {
+					// flag true로 설정
+					isMatched = true;
+					resultList.add(searchResult);
+					break;
+				}
+
+			} else if (key == 71 || key == 72 || key == 73 || key == 74 || key == 75 || key == 76 || key == 77
+					|| key == 78 || key == 79 || key == 710) {
+				Pattern pat = Pattern.compile("-.+");
+				Matcher match = pat.matcher(targetSentence);
+				
+				if (!match.find()) {
+					pat = Pattern.compile("_.+"); // 추가
+					match = pat.matcher(targetSentence);
+					if(match.find()) {
+						String normalizeWord = hm.get(key).replace("{}", match.group().substring(1));
+						JSONObject searchResult = searchTermObject(normalizeWord, "<71388002");
+						if (searchResult != null) {
+							// flag true로 설정
+							isMatched = true;
+							resultList.add(searchResult);
+							break;
+						}
+					}
+					
+				}else {
+					String normalizeWord = hm.get(key).replace("{}", match.group().split("-")[match.group().split("-").length-1]);
+					JSONObject searchResult = searchTermObject(normalizeWord, "<71388002");
+					if (searchResult != null) {
+						// flag true로 설정
+						isMatched = true;
+						resultList.add(searchResult);
+						break;
+					}
+				}
+				
+			} else if (key == 80) {   //80 룰 추가
+				targetSentence = mediCheckInfoList.get(0).getPreTerm().trim();
+				if(!targetSentence.isEmpty()){
+					List<JSONObject> searchResult = searchTermList(targetSentence, "<71388002");
+					for (JSONObject obj : searchResult) {
+						//후보 모두 저장
+						resultList.add(obj);
+					}
+				}
+			}
+			if (isMatched)
+				break;
+		}
+		return resultList;
+	}
+	
+	public List<JSONObject> mediCheck_autoRuleRequest(List<CmKexamVo> mediCheckInfoList){
+		List<JSONObject> list = new ArrayList<>();
+		
+		if(mediCheckInfoList.size() > 0){
+			list = autoRuleMapping(mediCheckInfoList);
+		}
+		
+		return list;
 	}
 
 }
